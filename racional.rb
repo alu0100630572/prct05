b7a79758595f0acc80a6baa214e83c2422a7f389
Rodrigo Colombo <alu0100630572@ull.edu.es>
2013-10-17 16:48:32 +0100

    Version 1

--- racional.rb
+++ racional.rb
@@ -1,6 +1,27 @@
 # Implementar en este fichero la clase para crear objetos racionales
+#! /bin/env ruby
 
 require "./gcd.rb"
 
 class Fraccion
+  
+  attr_reader :num, :denom
+  
+  def initialize(num,denom)  	#Metodo que es llamado por el constructor
+    @num, @denom = num,denom  
+  end
+  
+  def to_s			#Metodo para pasasr a string el resultado
+    "(#@num/#@denom)"
+  end
+  
+  def +(other)			#Metodo para sumar 
+    if (@denom == other.denom)
+      Fraccion.new(@num + other.num, @denom)
+    else if (@denom != other.denom)
+
+      
+  end
+  
+  def
 end
